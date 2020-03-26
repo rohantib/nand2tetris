8272e4792a5657ec9e7ba76fa977e4dc4837a332
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2020-03-26 12:04:18 -0700

    Finished Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,50 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(KBLOOP)
+    // Get Keyboard Input
+    @KBD
+    D=M     // Keyboard input is in D
+
+    // Set color to white or black
+    @WHITE
+    D;JEQ
+
+    // Color setting
+    @color
+    M=-1
+    @START
+    0;JMP
+(WHITE)
+    @color
+    M=0
+
+(START)
+    // Set start point in pos
+    @SCREEN
+    D=A
+    @pos
+    M=D
+
+    (SCRNLOOP)
+        // Set color
+        @color
+        D=M
+        @pos
+        A=M
+        M=D
+
+        // Increment pos pointer
+        @pos
+        M=M+1
+        // Check if at end of screen
+        @KBD
+        D=A
+        @pos
+        D=D-M
+        @SCRNLOOP
+        D;JGT
+
+    @KBLOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    // Ensure R2 is 0
+    @R2
+    M=0
+
+    // Grab one of the values and make it a counter
+    @R0
+    D=M
+    @i
+    M=D
+
+(LOOP)
+    // Keep adding R1 to R2 while i > 0
+    @i
+    D=M
+    @END
+    D;JLE // Terminate loop if i<= 0
+
+    // R2 += R1
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    // i -= 1
+    @i
+    M=M-1
+    
+    // Loop back
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
